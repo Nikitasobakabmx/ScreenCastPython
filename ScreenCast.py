8d084fd61bb25d51bcc911b7190c230f0579d9a0
Nikitosii <Nikitasobakabmx@gmail.com>
2019-07-11 22:49:29 +0300

    templates

---
+++ ScreenCast.py
@@ -0,0 +1,11 @@
+class ScreenCast:
+    def __init__(self):
+        pass
+    def mouseClick(self):
+        pass
+    def keybordClick(self):
+        pass
+    def ScreenCatcher(self):
+        pass
+    def videoWriter(self):
+        pass
