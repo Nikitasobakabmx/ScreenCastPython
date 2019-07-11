bf131a53a2a81a11c433ad46490427893ff01a97
Nikitosii <Nikitasobakabmx@gmail.com>
2019-07-11 22:57:18 +0300

    templates

--- ScreenCast.py
+++ ScreenCast.py
@@ -1,11 +1,16 @@
+import threading
+
 class ScreenCast:
     def __init__(self):
-        pass
+        self.mouseThred = threading.Thread(target = self.mouseClick)
+        self.keybordThred = threading.Thread(target = self.keybordClick)
+        self.screenThred = threading.Thread(target = self.screenCatcher)
+        self.videoWriterThred = threading.Thread(target = self.videoWriter)
     def mouseClick(self):
         pass
     def keybordClick(self):
         pass
-    def ScreenCatcher(self):
+    def screenCatcher(self):
         pass
     def videoWriter(self):
         pass
