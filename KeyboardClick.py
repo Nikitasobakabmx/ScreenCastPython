e8adb42cb04ccca73258ab62c3f14dd74d77c67c
PhilLar <phillarov@gmail.com>
2019-07-12 16:04:56 +0300

    code refinement

--- KeyboardClick.py
+++ KeyboardClick.py
@@ -1,6 +1,5 @@
 from pynput import keyboard
-import time
-#<0.07
+
 class KeyboardClick():
 
     def __init__(self):
@@ -20,12 +19,6 @@ class KeyboardClick():
                 if r in self.pressed:
                     self.pressed.remove(r)
                 self.released.remove(r)
-        # try:
-        # self.curr_time = time.time()
-        # self.diff_time = self.curr_time-self.prev_time
-        # self.prev_time = self.curr_time
-        # if self.diff_time > 0.07 or (self.released==key):
-        #     self.pressed = []
         if key not in self.pressed:
             self.pressed.append(key)
         
@@ -33,8 +26,8 @@ class KeyboardClick():
     def on_release(self, key):
         self.released.append(key)
         if key == keyboard.Key.esc:
-        # Stop listener
-            return False
+        # # Stop listener
+        #     return False
 
 # c = KeyboardClick()
 # c.start_listen()
