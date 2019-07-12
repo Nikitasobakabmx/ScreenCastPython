cf3b14789b21856fe2cdfd606eaaa54efbb9535b
PhilLar <phillarov@gmail.com>
2019-07-12 18:18:03 +0300

    changed listener

--- KeyboardClick.py
+++ KeyboardClick.py
@@ -4,30 +4,47 @@ class KeyboardClick():
 
     def __init__(self):
         self.pressed = []
-        self.prev_time = time.time()
         self.released = []
+        self.stop = False
     
-    def start_listen(self):
-        with keyboard.Listener(
-            on_press=self.on_press,
-            on_release=self.on_release) as listener:
-            listener.join()
+    
+
+
 
     def on_press(self, key):
-        while self.released:
-            for r in self.released:
-                if r in self.pressed:
-                    self.pressed.remove(r)
-                self.released.remove(r)
-        if key not in self.pressed:
-            self.pressed.append(key)
+        
+        # while self.released:
+        #     for r in self.released:
+        #         if r in self.pressed:
+        #             self.pressed.remove(r)
+        #         self.released.remove(r)
+        # if key not in self.pressed:
+        print('key', key)
+        self.pressed.append(key)
+        #print(self.pressed)
         
 
     def on_release(self, key):
         self.released.append(key)
-        if key == keyboard.Key.esc:
-        # # Stop listener
-        #     return False
+        # if key == keyboard.Key.esc:
+        #  # Stop listener
+        #     self.stop_listen()
 
-# c = KeyboardClick()
-# c.start_listen()
+    def start_listen(self):
+        listener = keyboard.Listener(
+            on_press=self.on_press,
+            on_release=self.on_release)
+        print('start')
+        listener.start()
+        try:
+            print('try')
+            listener.wait()
+        finally:
+            listener.stop()
+            #print(self.pressed)b bfdsf
+            return self.pressed
+
+c = KeyboardClick()
+for i in range(100000):
+    print(c.start_listen())
+# c.stop_listen()
