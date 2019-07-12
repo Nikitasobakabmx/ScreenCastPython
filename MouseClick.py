950868a3ae444775ea6694cfc3c1b0b5b544ab2e
unknown <vashelol30>
2019-07-12 16:34:13 +0300

    done

--- MouseClick.py
+++ MouseClick.py
@@ -8,20 +8,16 @@ class MouseClick():
 
     def on_move(self, x, y):
         self.pressed.update({'x' : x, 'y' : y})
-        print(self.pressed)
 
     def on_click(self, x, y, button, pressed):
         # try:
         if pressed:
             self.pressed.update({'button' : button.name})
-            print(self.pressed)
         if not pressed:
             self.pressed.update({'button' : None})
-            print(self.pressed)
 
     def on_scroll(self, x, y, dx, dy):
         self.pressed.update({'dy' : dy})
-        print(self.pressed)
 
     def start_listen(self):
         with mouse.Listener(
