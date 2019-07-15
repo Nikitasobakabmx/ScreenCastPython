20f859e0d0979b36482fe00b2f336361e6b6696d
Nikitosii <Nikitasobakabmx@gmail.com>
2019-07-15 12:40:36 +0300

    don't touch this monster

--- test.py
+++ test.py
@@ -1,83 +1,56 @@
 import cv2
-import threading
+import os
+from multiprocessing import Process, Queue
 import numpy as np
 import pyautogui
-
-from pynput import keyboard
-
-class KeyboardClick():
-
-    def __init__(self):
-        pass
-    
-    def start_listen(self):
-        with keyboard.Listener(
-            on_press=self.on_press) as listener:
-            listener.join()
-
-    def on_press(self, key):
-        # try:
-        self.pressed = []
-        self.pressed.append(key)
-        if key == keyboard.Key.esc:
-            return False
-
-    def on_release(self, key):
-        print('{0} released'.format(
-            key))
-        if key == keyboard.Key.esc:
-            # Stop listener
-            return False
+from PIL import Image
+from pynput import keyboard, mouse
+import time
 
 class ScreenCatcher:
     def __init__(self):
-        self.img = []
-        self.img.append(pyautogui.screenshot())
+        self.processes = []
+        self.q = Queue()
+        for _ in range(4):
+            self.processes.append(Process(target = self.shot))#args = self maybe
+        for process in self.processes:
+            process.start()
+        time.sleep(1)
+        self.bitrate = int(self.q.qsize() - self.q.qsize()/15) #magic 
     def shot(self):
-        a = 0
-        while a != 50:
-            tmpImg = pyautogui.screenshot()
-            self.img.append(tmpImg)
-            print("Shot")
-            a +=1
+        while True: #(time.time()*100 - startTime) < 95:
+            a = pyautogui.screenshot()
+            self.q.put({"pic" : a, "time" : time.time()*100})
+    def terminate(self):
+        for process in self.processes:
+            process.terminate()
 class VideoWriter:
     
-    def __init__(self, output = "video.avi", format = "mp4v"):
+    def __init__(self, outputFile = "video.avi", format = "mp4v"):
+        self.outputFile = outputFile
+        self.format = cv2.VideoWriter_fourcc(*format)
         self.SC = ScreenCatcher()
-        self.start(output, format)
+        self.bitrate = self.SC.bitrate
+        self.WriteProcess = Process(target = self.write, args = (self.SC.q, ))
 
-        self.catcherTread = threading.Thread(target=self.SC.shot())
-        self.VWriterTread = threading.Thread(target=self.write())
-
-        self.catcherTread.start()
-        self.VWriterTread.start()
-        self.catcherTread.join()
-        self.VWriterTread.join()
-
-    def write(self):
-        a = 0
-        while a < 50:
-            for img in self.SC.img:
-                img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
-                self.out.write(img)
-                self.SC.img = self.SC.img[1:]
-                print("write")
-            
-            a += 1
+    def write(self, q):
+        a = q.get()
+        a = cv2.cvtColor(np.array(a["pic"]), cv2.COLOR_RGB2BGR)
+        self.height, self.width, self.channels  = a.shape
+        self.out = cv2.VideoWriter(self.outputFile, self.format, self.bitrate, (self.width, self.height))
+        self.out.write(a)
+        startTime = timt.time() * 100
+        while (timt.time() * 100 - startTime) < 500: #expression
+            a = q.get()
+            a = cv2.cvtColor(np.array(a["pic"]), cv2.COLOR_RGB2BGR)
+            self.out.write(a)
+        self.save()
+        self.SC.terminate()
     def save(self):
-        self.flag = False
         self.out.release()
         cv2.destroyAllWindows()
-    def start(self, output = "video.avi", format = "mp4v"):
-        self.flag = True
-        self.output = output
-        self.format = cv2.VideoWriter_fourcc(*format)
-        self.SC.img[0] = cv2.cvtColor(np.array(self.SC.img[0]), cv2.COLOR_RGB2BGR)
-        self.height, self.width, self.channels = self.SC.img[0].shape
-        self.out = cv2.VideoWriter(self.output, self.format, 20.0, (self.width, self.height))
 
 if __name__ == "__main__":
     
     vW = VideoWriter()
     vW.write()
-    vW.save()
