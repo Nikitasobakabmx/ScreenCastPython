b607f9641d1b9c94521e4f5ae592cc67813e4845
unknown <vashelol30>
2019-07-16 16:17:48 +0300

    done

--- Mouse_and_Keyboard_Click.py
+++ Mouse_and_Keyboard_Click.py
@@ -24,8 +24,9 @@ class Redirector:
 
     def fire_events(self):
         while not self.queue.empty():
-            event = []
-            nowait = self.queue.get_nowait()['a']
+            elem = self.queue.get_nowait()
+            elem['to'](*elem['a'], **elem['kw'])
+            '''
             try:
                 if isinstance(nowait[3], bool):
                     event.append([nowait[2].name, nowait[0], nowait[1], nowait[3], None, time()])
@@ -41,6 +42,7 @@ class Redirector:
                     else:
                         event.append([None, None, None, None, nowait[0].name, time()])
             return(event)
+            '''
 
     def kill(self):
         self.should_work = False
@@ -54,30 +56,34 @@ def on_move(x, y):
 
 def on_click(x, y, button, pressed):
     pass
-    #print('{0} at {1}'.format(
-    #    'Pressed' if pressed else 'Released',
-    #    (x, y)))
+    print('{0} at {1}'.format(
+        'Pressed' if pressed else 'Released',
+        (x, y)))
 
 def on_scroll(x, y, dx, dy):
-    pass
-    #print('Scrolled {0} at {1}'.format(
-    #    'down' if dy < 0 else 'up',
-    #    (x, y)))
+    print('Scrolled {0} at {1}'.format(
+        'down' if dy < 0 else 'up',
+        (x, y)))
 
 def on_press(key):
-    pass
-    #print('alphanumeric key {0} pressed'.format(
-    #       key.char))
+    print('alphanumeric key {0} pressed'.format(
+           key.char))
 
 def on_release(key):
-    pass
-    #print('{0} released'.format(
-    #    key))
+    print('{0} released'.format(
+        key))
 
 mrd = Redirector(on_click=on_click, on_move=on_move, on_scroll=on_scroll)
 mrd.frequency = 11
 for _ in range(10000):
     sleep(0.01)
+<<<<<<< HEAD
+    mrd.fire_events()
+
+mrd.kill()
+
+=======
     print(mrd.fire_events())
 
 mrd.kill()
+>>>>>>> e491b3f81b58fd214b13c7a7b7bdef0b98e85c4e
