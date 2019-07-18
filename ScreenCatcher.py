0f67b644bce127c05c7c8c72f7f98a71f84ccf98
Nikitossii <nikitasobakabmx@gmail.com>
2019-07-18 15:59:45 +0300

    Tut screenCast

--- ScreenCatcher.py
+++ ScreenCatcher.py
@@ -6,24 +6,35 @@ import time
 from Redirector import Redirector
 
 class ScreenCatcher:
-    def __init__(self):
+    def __init__(self, redirector):
         print("start SC")
+        self.redirect = redirector
         self.q = Queue()        
         self.expression = True
+        
+
+        #count bitrate
+        startTime  = time.monotonic_ns()
+        self.q.put({"pic": pyautogui.screenshot(), "time": time.time() * 100, "position": self.redirect.position})
+        self.time = time.monotonic_ns() - startTime
+        self.bitrate = int(0.88*(10**9/self.time))
+        
         print("stop SC")
-        self.bitrate = 0
-        self.redirect = Redirector()
-        self.thread = Thread(target = self.redirect.start())
-        self.thread.start()
+
 
     def shot(self):
-        self.startTime = time.time() * 100
-        while (time.time()*100 - self.startTime) < 100:
-            self.q.put({"pic": pyautogui.screenshot(), "time": time.time() * 100, "position": self.redirect.position if self.redirect.position != None else (0,0)})
-        self.bitrate = self.q.qsize()
-        self.startTime = time.time() * 100
-        while (time.time()*100 - self.startTime) < 3000:
-            self.q.put({"pic": pyautogui.screenshot(), "time": time.time() * 100, "position": self.redirect.position if self.redirect.position != None else (0,0)})
+        print("start Shot")
+        loses = []
+        startTime = time.time() * 100
+        while (time.time()*100 - startTime) < 3000:
+            curStartTime = time.monotonic_ns()
+            self.q.put({"pic": pyautogui.screenshot(), "time": time.time() * 100, "position": self.redirect.position})
+            curTime = time.monotonic_ns() - curStartTime
+            if (self.time - curTime) > 0:
+                time.sleep((self.time - curTime)/10**9)
+            else:
+                loses.append(self.time - curTime)
         endTime = time.time() * 100
-        print("Shoting time : ", endTime - self.startTime, " sec * 10^-2")
+        print("Shoting time : ", endTime - startTime, " sec * 10^-2")
+        print(loses)
 
