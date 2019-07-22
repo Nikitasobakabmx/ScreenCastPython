50ea3bfe18fee9349c0e9dfc0ea36de24ecc2650
unknown <vashelol30>
2019-07-22 16:44:43 +0300

    hren

--- VideoWriter.py
+++ VideoWriter.py
@@ -3,33 +3,34 @@ from numpy import array
 from threading import Thread, Event
 from ScreenCatcher import ScreenCatcher
 from time import perf_counter
+
 try:
     import Image, ImageDraw, ImageFont
 except ImportError:
     from PIL import Image, ImageDraw, ImageFont
 
+
 class VideoWriter:
-    
+
     def __init__(self, outputFile="video.avi", format="mp4v"):
         self.mouse = Image.open("Images\\Mouse\\mouse.png")
         self.outputFile = outputFile
         self.format = cv2.VideoWriter_fourcc(*format)
-        
+
         self.keyList = {}
         f = open("key.txt", "r")
         for line in f:
             ln = line.split(" ")
-            self.keyList[ln[0]] = ln[1][:-1] 
+            self.keyList[ln[0]] = ln[1][:-1]
         f.close()
         self.expression = True
 
         self.ev = Event()
-        
+
         self.SC = ScreenCatcher(self.ev)
         self.SC.shotFactory()
-        
-        self.out = cv2.VideoWriter(self.outputFile, self.format, self.SC.bitrate, (self.SC.width, self.SC.height))
 
+        self.out = cv2.VideoWriter(self.outputFile, self.format, self.SC.bitrate, (self.SC.width, self.SC.height))
 
     def run(self):
         self.WriteProcess = Thread(target=self.write)
@@ -50,11 +51,11 @@ class VideoWriter:
             ImgSize = 0
             keys = queue[2]
             while keys != []:
-                #keyboard
+                # keyboard
                 if keys[0][0] != 'B':
                     queue[0].paste(self.mouse, (x_mouse, y_mouse), self.mouse)
-                    buffer.append({"key" : keys[0], "time" : perf_counter()})
-                #mouse
+                    buffer.append({"key": keys[0], "time": perf_counter()})
+                # mouse
                 else:
                     img = Image.open("Images\\Mouse\\" + str(keys[0]) + ".png")
                     queue[0].paste(img, (x_mouse, y_mouse), img)
@@ -71,19 +72,12 @@ class VideoWriter:
                     except FileNotFoundError:
                         img = Image.open("Images\\Keyboard\\Template.png")
                         draw = ImageDraw.Draw(img)
-<<<<<<< HEAD
-                        font = ImageFont.truetype("Images/Keyboard/Times_New_Roman.ttf", 20)
-                        draw.text((50 - len(str(i["key"]))*4, 45), str(i["key"]),(0, 0, 0),font=font)
-                        img.save('Images/Keyboard/' + (str(i["key"])) + ".png")
-                        self.keyList[i["key"]] = "Images/Keyboard/" + str(i["key"]) + ".png"
-=======
-                        font = ImageFont.truetype("Images\\Keyboard\\Times_New_Roman.ttf", 30)
-                        draw.text((60 - len(str(i["key"])) * 5, 55), str(i["key"]),(0, 0, 0),font=font)
+                        font = ImageFont.truetype("Images\\Keyboard\\Times_New_Roman.ttf", 20)
+                        draw.text((50 - len(str(i["key"])) * 4, 45), str(i["key"]), (0, 0, 0), font=font)
                         img.save('Images\\Keyboard\\' + (str(i["key"])) + ".png")
                         self.keyList[i["key"]] = "Images\\Keyboard\\" + str(i["key"]) + ".png"
->>>>>>> dbdc251e51008209bfd5d88ed0a4ac560ee77956
                         img = Image.open(self.keyList[i["key"]])
-                    ImgSize += 125
+                    ImgSize += 160
                     queue[0].paste(img, (x, y), img)
             queue[0] = cv2.cvtColor(array(queue[0]), cv2.COLOR_RGB2BGR)
             self.out.write(queue[0])
@@ -102,6 +96,6 @@ class VideoWriter:
         f.close()
 
     def save(self):
-        #self.redirect.kill()
+        # self.redirect.kill()
         self.out.release()
         cv2.destroyAllWindows()
